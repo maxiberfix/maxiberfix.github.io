e42d85bf7e79deaf0ecb74a827906437a6e5ff1b
maxipalay <maximilianopalay@gmail.com>
2019-10-25 14:06:04 -0300

    m

--- js/iberfix.js
+++ js/iberfix.js
@@ -1,3 +1,19 @@
 $('.carousel').carousel({
-interval: 6000
+interval: 4000
 })
+
+new WOW().init();
+
+// codigo adaptado de https://stackoverflow.com/questions/25369487/background-image-that-moves-when-you-scroll-down-the-page
+var velocity = 0.2;
+function update(){ 
+var pos = $(window).scrollTop(); 
+$('.carousel-item').each(function() { 
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height();
+    $(this).css('backgroundPosition', '50%' + Math.round(pos * velocity)+  'px'); 
+   }); 
+   };
+
+ $(window).bind('scroll', update);
